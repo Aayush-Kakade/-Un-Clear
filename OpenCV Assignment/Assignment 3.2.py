1b57be7b5b00ec4c1673694f09fdb3a03be19e0a
Aayush Kakade <81591596+Aayush-Kakade@users.noreply.github.com>
2021-04-11 22:56:42 +0530

    Update Assignment 3.2.py

--- OpenCV Assignment/Assignment 3.2.py
+++ OpenCV Assignment/Assignment 3.2.py
@@ -9,12 +9,12 @@ while(1):
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     img_invert = cv2.bitwise_not(img_gray)
-    img_smooth = cv2.GaussianBlur(img_invert, (21,21), sigmaX=0, sigmaY=0)
-    def dodgeV2(x,y):
+    img_smooth = cv2.GaussianBlur(img_invert, (21,21),0)
+    def finalsketch(x,y):
         return cv2.divide(x,255-y, scale=256)
 
-    image_fn = dodgeV2(img_gray, img_smooth)
-    cv2.imshow("Final Feed",image_fn)
+    output = finalsketch(img_gray, img_smooth)
+    cv2.imshow("Final Feed",output)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release
